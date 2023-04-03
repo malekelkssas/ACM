d96f13e48501dcc9ff89c88358fc395908f23dc0
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-04-03 14:35:06 +0200

    adding Math package

---
+++ src/Math/bitwise.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class bitwise {
+
+	public void xor()
+	{
+		/*		1- the basic properties of the Xor operation: A^0=A, A^A=0 A^B=B^A
+		 * 		2- (a1^x)^(a2^x)^.^.^. equals a1^a2^. . .^an 	if n is even
+		 * 		and- a1 ^ a2^. . .an ^ x 						if n is odd
+		 */
+	}
+}
